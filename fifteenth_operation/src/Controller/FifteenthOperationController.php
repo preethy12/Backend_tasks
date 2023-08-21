9abdbfa67cbad007a0f347f9834763b253414d7e
preethy12 <preethyray12@.com>
2023-08-21 21:21:19 +0530

    sds

---
+++ fifteenth_operation/src/Controller/FifteenthOperationController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\fifteenth_operation\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for Fifteenth operation routes.
+ */
+class FifteenthOperationController extends ControllerBase {
+
+  /**
+   * Displays the title of the node.
+   */
+  public function build($node) {
+    // $node = Node::load(10);
+    return [
+      '#markup' => $node->getTitle(),
+    ];
+  }
+
+}
